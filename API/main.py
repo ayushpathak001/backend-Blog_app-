12d34a624947d7d227000a5136a42e86b73eefb5
Ayush Pathak <ayushpathak00851@gmail.com>
2026-09-21 14:34:46 +0530

    Complete password reset and email logo integration
    Completed JWT-based password reset flow
    Added reset token validation and user verification
    Added secure password hashing before database update
    Fixed MongoDB ObjectId serialization issue
    Added proper success and error responses
    Updated email template with a publicly hosted logo URL
    Improved email logo visibility across different devices and email clients

--- API/main.py
+++ API/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from routes import auth , users , password_reset
-
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+# Serve static files 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/" , tags=["Check Route API"])
 def read_root():
@@ -15,8 +17,4 @@ def read_root():
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(password_reset.router)
-
-
-
-
 
